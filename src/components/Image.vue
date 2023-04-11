b2dd9ca192f4248a180ff97324af028587048aed
Gyarmati Levente <gyarmatilevi@gmail.com>
2023-04-11 21:43:49 +0200

    image view

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -8,17 +8,8 @@
     height="100%"
   >
     <v-carousel-item
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      cover
-    ></v-carousel-item>
-
-    <v-carousel-item
-      src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
-      cover
-    ></v-carousel-item>
-
-    <v-carousel-item
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      v-for="image in images"
+      :src="image"
       cover
     ></v-carousel-item>
   </v-carousel>
@@ -43,14 +34,18 @@
       </v-carousel>
     </v-row>-->
     <v-row>
-      <v-col sm="1" cols="1"> <v-btn icon="mdi-chevron-left"></v-btn></v-col
-      ><v-col sm="10" cols="10"
-        ><v-img
-          src="../../public/haz.jpg"
-          width="80vw"
+      <v-col sm="1" cols="1">
+        <v-btn v-on:click="previous()" icon="mdi-chevron-left"></v-btn></v-col
+      ><v-col sm="10" cols="10">
+        <v-img
+          :src="this.images[this.index]"
+          width="60vw"
           aspect-radio="16/9" /></v-col
       ><v-col sm="1" cols="1"
-        ><v-btn icon="mdi-chevron-right"></v-btn></v-col></v-row
+        ><v-btn
+          v-on:click="next()"
+          icon="mdi-chevron-right"
+        ></v-btn></v-col></v-row
   ></v-overlay>
 </template>
 
@@ -62,10 +57,30 @@ export default defineComponent({
   data() {
     return {
       number: null,
+      index: null,
       overlay: null,
+      images: [
+        "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+        "https://cdn.vuetifyjs.com/images/cards/hotel.jpg",
+        "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg",
+      ],
     };
   },
   methods: {
+    next() {
+      if (this.index == this.images.length - 1) {
+        this.index = 0;
+      } else {
+        this.index++;
+      }
+    },
+    previous() {
+      if (this.index == 0) {
+        this.index = this.images.length - 1;
+      } else {
+        this.index--;
+      }
+    },
     check(event) {
       if (event.target.src === undefined) {
         console.log("nincs");
@@ -73,6 +88,7 @@ export default defineComponent({
         this.overlay = true;
         console.log(event);
         console.log(this.number);
+        this.index = this.number;
         console.log(event.target.src);
       }
     },
